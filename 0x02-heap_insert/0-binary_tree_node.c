653886c6b419e9b7dcb3dd1638c9345e1adbfa8a
scan3ls <scan3ls@gmail.com>
2020-12-22 10:23:02 -0600

    minor fixes

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -11,7 +11,7 @@
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new_node, iterator;
+    binary_tree_t *new_node;
 
     if (parent == NULL)
     {
@@ -19,23 +19,31 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
         return (new_node);
     }
 
-    new_node = malloc(sizeof(binary_tree_t *));
+    new_node = malloc(sizeof(binary_tree_t));
     if (new_node == NULL)
         return (NULL);
 
     new_node->n = value;
-    new_node->parent parent;
+    new_node->parent = parent;
     new_node->left = NULL;
     new_node->right = NULL;
 
     return (new_node);
 }
 
+/**
+ * create_new_tree - create a new binary tree with head node
+ *
+ *@value: int value of new head node
+ *
+ * Return: pointer to created node or NULL
+ */
+
 binary_tree_t *create_new_tree(int value)
 {
-    binary_tree_t *new_node = malloc(sizeof(binary_tree_t *));
+    binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
 
-    if (*new_node == NULL)
+    if (new_node == NULL)
         return (NULL);
 
     new_node->n = value;
