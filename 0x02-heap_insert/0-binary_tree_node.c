858939577a172a3b71b9ac19751f071fdd639846
scan3ls <scan3ls@gmail.com>
2020-12-22 10:24:20 -0600

    Style fixes

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -11,24 +11,24 @@
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new_node;
+	binary_tree_t *new_node;
 
-    if (parent == NULL)
-    {
-        new_node = create_new_tree(value);
-        return (new_node);
-    }
+	if (parent == NULL)
+	{
+		new_node = create_new_tree(value);
+		return (new_node);
+	}
 
-    new_node = malloc(sizeof(binary_tree_t));
-    if (new_node == NULL)
-        return (NULL);
+	new_node = malloc(sizeof(binary_tree_t));
+	if (new_node == NULL)
+		return (NULL);
 
-    new_node->n = value;
-    new_node->parent = parent;
-    new_node->left = NULL;
-    new_node->right = NULL;
+	new_node->n = value;
+	new_node->parent = parent;
+	new_node->left = NULL;
+	new_node->right = NULL;
 
-    return (new_node);
+	return (new_node);
 }
 
 /**
@@ -41,15 +41,15 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 
 binary_tree_t *create_new_tree(int value)
 {
-    binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
 
-    if (new_node == NULL)
-        return (NULL);
+	if (new_node == NULL)
+		return (NULL);
 
-    new_node->n = value;
-    new_node->parent = NULL;
-    new_node->left = NULL;
-    new_node->right = NULL;
+	new_node->n = value;
+	new_node->parent = NULL;
+	new_node->left = NULL;
+	new_node->right = NULL;
 
-    return (new_node);
+	return (new_node);
 }
