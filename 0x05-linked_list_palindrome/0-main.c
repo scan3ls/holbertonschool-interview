411ff9a02346ac0fb827ba7caec893fd2ce9bb46
scan3ls <scan3ls@gmail.com>
2021-01-18 10:45:28 -0600

    Make test file more usable

--- 0x05-linked_list_palindrome/0-main.c
+++ 0x05-linked_list_palindrome/0-main.c
@@ -13,31 +13,38 @@ void build_test_list(listint_t **head, int *test);
  */
 int main(void)
 {
-    int test[TEST_LENGTH] = {
-        1, 17, 972, 50, 98,
-        98, 50, 972, 17, 1
-    };
+	int test[TEST_LENGTH] = {
+		1, 17, 972, 50, 98,
+		98, 50, 972, 17, 1
+	};
 
-    listint_t *head;
+	listint_t *head;
 
-    head = NULL;
-    build_test_list(&head, test);
-    print_listint(head);
+	head = NULL;
+	build_test_list(&head, test);
+	print_listint(head);
 
-    if (is_palindrome(&head) == 1)
-        printf("Linked list is a palindrome\n");
-    else
-        printf("Linked list is not a palindrome\n");
+	if (is_palindrome(&head) == 1)
+		printf("Linked list is a palindrome\n");
+	else
+		printf("Linked list is not a palindrome\n");
 
-    free_listint(head);
+	free_listint(head);
 
-    return (0);
+	return (0);
 }
 
-void build_test_list( listint_t **head, int *test)
-{
-    int index;
+/**
+ * build_test_list - builda a linked list from an array
+ *@head: head node of the linked list
+ *@test: array with test values
+ *
+ */
 
-    for( index = 0; index < TEST_LENGTH; index++ )
-        add_nodeint_end(head, test[index]);
+void build_test_list(listint_t **head, int *test)
+{
+	int index;
+
+	for (index = 0; index < TEST_LENGTH; index++)
+		add_nodeint_end(head, test[index]);
 }
