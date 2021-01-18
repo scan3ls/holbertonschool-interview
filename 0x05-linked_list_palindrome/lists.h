b129ba66e11c585b57ea550718d34045c0675df7
scan3ls <scan3ls@gmail.com>
2021-01-18 10:47:28 -0600

    Add helper functions

--- 0x05-linked_list_palindrome/lists.h
+++ 0x05-linked_list_palindrome/lists.h
@@ -33,5 +33,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 
 int is_palindrome(listint_t **head);
+int get_length(listint_t **head);
+void build_list( int *list, listint_t **head );
 
 #endif /* LISTS_H */
