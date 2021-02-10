e0184cb4b4463059a076e58547ab119371cd395b
scan3ls <scan3ls@gmail.com>
2021-02-09 19:09:45 -0600

    Rewrite for boomer status

--- 0x08-palindrome_integer/0-is_palindrome.c
+++ 0x08-palindrome_integer/0-is_palindrome.c
@@ -1,7 +1,7 @@
 #include "palindrome.h"
-
+#include <stdio.h>
 /**
- * is_palindrome - [ DRIVER ] for checking if n is a palindrome
+ * is_palindrome - build the number backwards and check if it is the same
  *@n: unsigned long int to check
  *
  * Return: true/false
@@ -9,50 +9,13 @@
 
 int is_palindrome(unsigned long n)
 {
-	num_t num = { n, 0, {0} };
-	bool result;
+	unsigned long backwards = 0;
+	unsigned long num;
 
-	ltoa(&num);
-	result = check_palindrome(&num);
-
-	return (result);
-}
-
-/**
- *	ltoa - converts long to array while counting number length
- *@num: long number to convert
- *
- */
-
-void ltoa(num_t *num)
-{
-	unsigned long n = num->n;
-
-	for (num->length = 0; n > 0; num->length++, n /= 10)
+	for (num = n; num > 0; num = num / 10)
 	{
-		num->digits[num->length] = n % 10;
-	}
-}
-
-/**
- * check_palindrome - checks if number is a palindrome
- *@num: number to check
- *
- * Return: true false
- */
-
-bool check_palindrome(num_t *num)
-{
-	int start = 0;
-	int end = num->length - 1;
-
-	while (start < end)
-	{
-		if (num->digits[start] != num->digits[end])
-			return (false);
-		start++;
-		end--;
+		backwards = (backwards * 10) + (num % 10);
 	}
 
-	return (true);
+	return (backwards == n);
 }
--- 0x08-palindrome_integer/palindrome.h
+++ 0x08-palindrome_integer/palindrome.h
@@ -3,38 +3,7 @@
 
 #include <stdio.h>
 
-#define MAX_SIZE 20 /* Max array size for unsigned long*/
-
-/**
- * struct num_s - contains number data
- *@n: value of number as unsigned long
- *@length: number of digits
- *@digits: array container for the digits
- */
-
-typedef struct num_s
-{
-	unsigned long n;
-	int length;
-	int digits[MAX_SIZE];
-} num_t;
-
-/**
- * enum boolean - defines true false booleans
- *@false: 0 = false
- *@true: 1 = true
- */
-
-enum boolean
-{
-	false, true
-};
-
-typedef int bool;
-
 /* 0-is_palindrome.c */
 int is_palindrome(unsigned long n);
-void ltoa(num_t *num);
-bool check_palindrome(num_t *num);
 
 #endif /* PALINDROME_H*/
