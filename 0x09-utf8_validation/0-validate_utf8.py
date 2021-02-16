e0199cfe8e1fa106ac9a5456208d4870e80925f5
scan3ls <scan3ls@gmail.com>
2021-02-16 17:13:03 -0600

    Verify UTF-8 Rules

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -6,9 +6,34 @@
 
 def validUTF8(data):
     """ Validator """
-    try:
-        bytes(data)
-    except ValueError:
+    while (data):
+        character = getCharacter(data)
+        if character is False:
+            return False
+    return True
+
+
+def getCharacter(data):
+    octet_seq = []
+    byte = "{:08b}".format(data.pop(0))
+
+    if byte[0] == '0':
+        return byte
+
+    octet_seq.append(byte)
+
+    start = byte.split('0')[0]
+    length = start.count('1') - 1
+    if length == 0 or length >= 4:
         return False
 
-    return True
+    for _ in range(length):
+        if data == []:
+            return False
+        byte = "{:08b}".format(data.pop(0))
+        if byte[0:2] != "10":
+            return False
+
+        octet_seq.append(byte)
+
+    return octet_seq
