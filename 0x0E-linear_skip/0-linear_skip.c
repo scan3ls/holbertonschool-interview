8f1deeb1308c8f47123d5d7cdfc3cb5c237a7d9d
scan3ls <scan3ls@gmail.com>
2021-03-29 04:59:52 -0500

    Add file

---
+++ 0x0E-linear_skip/0-linear_skip.c
@@ -0,0 +1,53 @@
+#include "search.h"
+
+/**
+ * linear_skip - quickly searches linked list
+ * @list: head node
+ * @value: target value
+ *
+ * Return: Node with the given target value or NULL
+ */
+
+skiplist_t *linear_skip(skiplist_t *list, int value)
+{
+	skiplist_t *temp = list;
+	unsigned long length;
+
+	if (list == NULL)
+		return (NULL);
+	if (temp->express)
+		length = temp->express->n * temp->express->n;
+
+	while (temp->express)
+	{
+		printf(
+			"Value checked at index [%lu] = [%d]\n",
+			temp->express->index,
+			temp->express->n
+		);
+		if (temp->n == value)
+			return (temp);
+		if (temp->express->n > value)
+			break;
+		temp = temp->express;
+	}
+
+	printf(
+		"Value found between indexes [%lu] and [%lu]\n",
+		temp->index,
+		(temp->express) ? temp->express->index : --length
+	);
+
+	while (temp)
+	{
+		printf(
+			"Value checked at index [%lu] = [%d]\n",
+			temp->index,
+			temp->n
+		);
+		if (temp->n == value)
+			return (temp);
+		temp = temp->next;
+	}
+	return (NULL);
+}
