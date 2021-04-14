374c3e214f214cce525a232b217a5ef194321592
scan3ls <scan3ls@gmail.com>
2021-04-14 14:03:14 -0500

    Rewrite w/o Classes

--- 0x10-rain/0-rain.py
+++ 0x10-rain/0-rain.py
@@ -1,46 +1,33 @@
 #!/usr/bin/python3
+""" Find space between walls"""
 
-class TheRain():
-    """
-        Find the space between the walls
-    """
-    _COLLECTED = 0
+def build_walls(walls):
+    """ find water levels """
+    levels = []
+    for height in range(max(walls), 0, -1):
+        level = []
+        for index, wall in enumerate(walls):
+            if wall >= height:
+                level.append(str(index))
+            else:
+                level.append(' ')
+        levels.append(level.copy())
 
-    def __init__(self, walls):
-        """ constructor """
-        self.walls = walls.copy()
-        self._tallest_wall = max(walls)
-        self._levels = []
-
-    def _find_levels(self):
-        """ find water levels """
-        for height in range(self._tallest_wall, 0, -1):
-            level = []
-            for index, wall in enumerate(self.walls):
-                if wall >= height:
-                    level.append(str(index))
-                else:
-                    level.append(' ')
-            self._levels.append(level.copy())
-
-    def falls(self):
-        """ Finds space between walls """
-        self._find_levels()
-        # print(self.walls)
-        for level in self._levels:
-            s1 = "".join(level)
-            # print(s1.replace(' ', '_'))
-            s1 = s1.strip()
-            # print(s1.replace(' ', '_'))
-            self._COLLECTED += s1.count(' ')
-        return self._COLLECTED
-
+    return levels
 
 def rain(walls):
-    """ rain function """
+    """ rain function Finds space between walls """
     if walls == []:
         return 0
 
-    the_rain = TheRain(walls)
-    result = the_rain.falls()
-    return result
+    levels = build_walls(walls)
+    collected = 0
+
+    for level in levels:
+        s1 = "".join(level)
+        # print(s1.replace(' ', '_'))
+        s1 = s1.strip()
+        # print(s1.replace(' ', '_'))
+        collected += s1.count(' ')
+
+    return collected
