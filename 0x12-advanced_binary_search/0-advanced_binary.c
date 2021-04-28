22a7d0da27e171e8aea4ab51f97be2e32d473c34
scan3ls <scan3ls@gmail.com>
2021-04-28 13:30:50 -0500

    Add file

---
+++ 0x12-advanced_binary_search/0-advanced_binary.c
@@ -0,0 +1,66 @@
+#include "search_algos.h"
+
+/**
+ * advanced_binary - driver for searching an array
+ *                   using the search function
+ *@array: array of integers
+ *@size: length of array
+ *@value: target value to find
+ * Return: index of target value in array or -1
+ */
+int advanced_binary(int *array, size_t size, int value)
+{
+	int found = search(array, size, value);
+
+	return ((found >= 0) ? found : -1);
+}
+
+/**
+ * search - recursive function to search array
+ *          for a specific integer
+ *@array: array of integers
+ *@size: length of array
+ *@value: target value to find
+ * Return: index of target value in array
+ */
+int search(int *array, size_t size, int value)
+{
+	int *step, index = 0;
+	int half = (float)size / 2.0 + 0.5;
+
+	print_array(array, size);
+	if (*array == value)
+		return (index);
+
+	if (size < 2 && *array != value)
+		return (INT_MIN);
+
+	step = array + (half);
+
+	if (value == *(step - 1))
+		return (search(array, half, value));
+
+	if (value >= *step)
+		return (search(step, size - half, value) + half);
+	return (search(array, half, value));
+}
+
+/**
+ * print_array - Print an array of integers
+ *@array: array of integers
+ *@size: length of array
+ */
+void print_array(int *array, size_t size)
+{
+	int i, j;
+	char *str = malloc(sizeof(int) * size);
+
+	for (i = 0, j = 0; i < (int)size; i++)
+	{
+		str[j++] = array[i] + '0';
+		str[j++] = ',';
+		str[j++] = ' ';
+	}
+	str[j - 2] = '\0';
+	printf("Searching in array: %s\n", str);
+}
---
+++ 0x12-advanced_binary_search/search_algos.h
@@ -0,0 +1,12 @@
+#ifndef _SEARCH_ALGOS_H_
+#define _SEARCH_ALGOS_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+int advanced_binary(int *array, size_t size, int value);
+void print_array(int *, size_t);
+int search(int *, size_t, int);
+
+#endif /* _SEARCH_ALGOS_H_ */
