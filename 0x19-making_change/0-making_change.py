ce59946130f970e27dbf4e616acf114c8dd5581c
scan3ls <scan3ls@gmail.com>
2021-06-23 03:21:55 -0500

    Add file

---
+++ 0x19-making_change/0-making_change.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+""" Return fewest number of coins for change """
+
+def invalidArguments(coins, total):
+    """ validate args"""
+    if isinstance(coins, list) is False:
+        return True
+    if isinstance(total, int) is False:
+        return True
+    if total <= 0:
+        return True
+
+    all_ints = True if list(map(lambda x: isinstance(x, int), coins)).count(False) == 0 else False
+    if all_ints is False:
+        return True
+
+    return False
+
+
+def makeChange(coins, total):
+    if invalidArguments(coins, total):
+        return 0
+
+    coins.sort()
+    coins.reverse()
+    counter = 0
+
+    while coins != []:
+        divisor = coins[0]
+        if isinstance(divisor, int) is False:
+            continue
+        if divisor <= 0:
+            continue
+
+        if total < divisor:
+            coins.pop(0)
+            continue
+
+        result = int(total / divisor)
+        counter = counter + result
+        total = total - (result * divisor)
+
+    return counter if total == 0 else -1
+
+if __name__ == "__main__":
+    print(makeChange(1, 37))
+    print(makeChange([1], 'yes'))
+    print(makeChange([1, 'a', -1], 100))
+    print(makeChange([1, 25, 2], 37))
+    print(makeChange([1256, 54, 48, 16, 102], 1453))
---
+++ 0x19-making_change/README.md
@@ -0,0 +1,6 @@
+# 0x19. making_change
+
+## Introduction
+This repository is used for project based learning in Holberton.
+
+## Learning Objectives
