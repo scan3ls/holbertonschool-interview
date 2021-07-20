794e26225663b1bbaee419eee2b9d80b3f550863
scan3ls <scan3ls@gmail.com>
2021-07-19 23:22:27 -0500

    Update

--- 0x1D-avl_trees/binary_trees.h
+++ 0x1D-avl_trees/binary_trees.h
@@ -1,6 +1,8 @@
 #ifndef BINARY_TREES_H
 #define BINARY_TREES_H
 
+#include <stdlib.h>
+
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -11,15 +13,19 @@
  */
 struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 };
 
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s binary_tree_t;
 
+binary_tree_t *binary_tree_node(binary_tree_t *, int value);
+void binary_tree_print(const binary_tree_t *tree);
+
 int binary_tree_is_avl(const binary_tree_t *tree);
+int get_height(const binary_tree_t *tree);
 
 #endif
