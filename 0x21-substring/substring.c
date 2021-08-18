1f45d929567cf8222e785cef2a6fb07520f3233c
scan3ls <scan3ls@gmail.com>
2021-08-18 06:03:11 -0500

    Add file

---
+++ 0x21-substring/substring.c
@@ -0,0 +1,18 @@
+#include "substring.h"
+
+/**
+ * find_substring - does stuff
+ * @s: src string
+ * @words: array of strings of same length
+ * @nb_words: length of array of words
+ * @n: location storing number of incdices found
+ *
+ * Return: array of indices as ints
+ */
+int *find_substring(char const *s, char const **words, int nb_words, int *n)
+{
+	int *indices = malloc(sizeof(int) * 1);
+
+	*n = 1;
+	return (indices);
+}
